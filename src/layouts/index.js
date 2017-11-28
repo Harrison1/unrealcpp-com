577abc64c396e9778949f22f52c318737cd63b26
Harrison <hmcguire13@gmail.com>
2017-11-28 07:29:00 -0500

    added two posts

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
-import 'prismjs/themes/prism-okaidia.css'
+import '../css/prism-okaidia.css'
 import '../css/global.css'
 import '../css/screen.css'
 
