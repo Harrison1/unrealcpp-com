7bca8b9739069f329a0d57811c8537e185fb40e9
Harrison <hmcguire13@gmail.com>
2017-12-22 12:59:06 -0500

    data update

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -5,11 +5,11 @@ module.exports = {
     siteLogo: "https://res.cloudinary.com/several-levels/image/upload/v1512476066/unreal-cpp-logo_izqxay.png", // Logo used for SEO and manifest.
     siteUrl: "https://unrealcpp.com/", // Domain of your website without pathPrefix.
     pathPrefix: "/", // Prefixes all links. For cases when deployed to example.github.io/gatsby-advanced-starter/.
-    siteDescription: "A tutorial site dedicated to using C++ in Unreal Engine 4.  This site is meant for game developers wanting to learn how to begin using c++ in UE4.", // Website description used for RSS feeds/meta description tag.
+    siteDescription: "A tutorial site dedicated to using C++ in Unreal Engine 4. This site is meant for game developers wanting to learn how to begin using c++ in UE4.", // Website description used for RSS feeds/meta description tag.
     siteRss: "/rss.xml", // Path to the RSS file.
-    googleAnalyticsID: "UA-47311644-5", // GA tracking ID.
+    googleAnalyticsID: "UA-87708839-4", // GA tracking ID.
     userName: "Harrison McGuire", // Username to display in the author segment.
-    copyright: "Copyright © 2017. UnrealCPP.com", // Copyright string for the footer of the website and RSS feed.
+    copyright: "Copyright © 2017. unrealcpp.com", // Copyright string for the footer of the website and RSS feed.
     themeColor: "#663399", // Used for setting manifest and progress theme colors.
     backgroundColor: "#0086b3" // Used for setting manifest background color.
   }
