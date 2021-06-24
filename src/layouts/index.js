985b6479d6df0230ab27f6e6aa80db55558081e7
Harrison McGuire <hmcguire13@gmail.com>
2021-06-24 12:13:58 -0400

    updated helmet

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -14,33 +14,41 @@ import favicon16 from '../favicons/favicon-16x16.png'
 
 const TemplateWrapper = ({ children }) => (
   <div>
-    <Helmet
-      title="Unreal C++"
-      meta={[
-        { name: 'description', content: 'A tutorial site dedicated to using C++ in Unreal Engine 4. This site is meant for game developers wanting to learn how to begin using c++ in UE4.' },
-        { name: 'keywords', content: 'unreal, engine, ue4, c++, cpp, game, development, harrison, mcguire' },
-        { charSet: 'utf-8'},
-        { name: 'theme-color', content: '#663399'}
-      ]}
-      link={[
-        { rel: 'apple-touch-icon', sizes: "57x57", href: appleIcon57 },
-        { rel: 'apple-touch-icon', sizes: "60x60", href: appleIcon60 },
-        { rel: 'apple-touch-icon', sizes: "72x72", href: appleIcon72 },
-        { rel: 'apple-touch-icon', sizes: "76x76", href: appleIcon76 },
-        { rel: 'apple-touch-icon', sizes: "114x114", href: appleIcon114 },
-        { rel: 'apple-touch-icon', sizes: "120x120", href: appleIcon114 },
-        { rel: 'apple-touch-icon', sizes: "144x144", href: appleIcon114 },
-        { rel: 'apple-touch-icon', sizes: "152x152", href: appleIcon152 },
-        { rel: 'apple-touch-icon', sizes: "180x180", href: appleIcon180 },
-        { rel: 'icon', type: "image/png", sizes: "192x192", href: androidChrome192 },
-        { rel: 'icon', type: "image/png", sizes: "32x32", href: favicon32 },
-        { rel: 'icon', type: "image/png", sizes: "96x96", href: favicon96 },
-        { rel: 'icon', type: "image/png", sizes: "16x16", href: favicon16 },
-        { rel: 'canonical', href: 'https://unrealcpp.com' }
-      ]}
-    />
+    <Helmet>
+      <title>Unreal C++</title>
+      <meta name='description' content='A tutorial site dedicated to using C++ in Unreal Engine 4. This site is meant for game developers wanting to learn how to begin using c++ in UE4.' />
+      <meta name='keywords' content='unreal, engine, ue4, c++, cpp, game, development, harrison, mcguire' />
+      <meta charSet='utf-8' />
+      <meta name='theme-color' content='#663399' />
 
-    <div>{ children }</div>
+      <link rel='apple-touch-icon' sizes='57x57' href={appleIcon57} />
+      <link rel='apple-touch-icon' sizes='60x60' href={appleIcon60} />
+      <link rel='apple-touch-icon' sizes='72x72' href={appleIcon72} />
+      <link rel='apple-touch-icon' sizes='76x76' href={appleIcon76} />
+      <link rel='apple-touch-icon' sizes='114x114' href={appleIcon114} />
+      <link rel='apple-touch-icon' sizes='120x120' href={appleIcon114} />
+      <link rel='apple-touch-icon' sizes='144x144' href={appleIcon114} />
+      <link rel='apple-touch-icon' sizes='152x152' href={appleIcon152} />
+      <link rel='apple-touch-icon' sizes='180x180' href={appleIcon180} />
+
+      <link rel='icon' type='image/png' sizes='192x192' href={androidChrome192} />
+      <link rel='icon' type='image/png' sizes='32x32' href={favicon32} />
+      <link rel='icon' type='image/png' sizes='96x96' href={favicon96} />
+      <link rel='icon' type='image/png' sizes='16x16' href={favicon16} />
+
+      <link rel='canonical' href='https://unrealcpp.com' />
+
+      <script async src='https://www.googletagmanager.com/gtag/js?id=G-PSL3Q0PR40'></script>
+    </Helmet>
+    <Helmet
+      script={[{ 
+        type: 'text/javascript', 
+        innerHTML: `window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-PSL3Q0PR40');`
+      }]}
+    />
+    <div>
+      { children }
+    </div>
   </div>
 );
 
